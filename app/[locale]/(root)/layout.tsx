47cceda7df37cdf619bd358855126569cdec08ad
ShevaWork <olexandr.sheva@gmail.com>
2025-03-17 13:30:47 +0200

    Update layout.tsx

--- app/[locale]/(root)/layout.tsx
+++ app/[locale]/(root)/layout.tsx
@@ -107,9 +107,9 @@ export default async function RootLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  params: { locale: string };
 }) {
-  const { locale } = await params;
+  const { locale } = params;
   if (!routing.locales.includes(locale as Locale)) {
     notFound();
   }
